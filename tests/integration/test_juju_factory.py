tonyandrewmeyer/pytest-jubilant

commit 452ef35f37e8ef1e395c5c2a0ee402186c753c1a

feat: add support for cloud and controller in juju_factory fixture (#67)
This adds support for passing controller or cloud name given those
arguments are supported by `jubilant`.
This extends the capabilities of the `juju_factory` so anyone can tests
CMR between different type of charms (k8s vs machine).

Resolves #66 

---------

Co-authored-by: James Garner <james.garner@canonical.com>

pytest_jubilant/_main.py
@@ -46,6 +46,18 @@ def pytest_addoption(parser: pytest.Parser):
         help="Prefix for Juju model names.",
     )
     group.addoption(
+        "--juju-controller",
+        action="store",
+        default=None,
+        help="The default Juju controller to use for tests.",
+    )
+    group.addoption(
+        "--juju-cloud",
+        action="store",
+        default=None,
+        help="The default Juju cloud to use for tests.",
+    )
+    group.addoption(
         "--no-juju-setup",
         action="store_true",
         default=False,
@@ -149,13 +161,24 @@ class JujuFactory(typing.Protocol):
     factory (see the ``juju_factory`` fixture).
     """
 
-    def get_juju(self, suffix: str) -> jubilant.Juju:
+    def get_juju(
+        self,
+        suffix: str,
+        *,
+        controller: str | None = None,
+        cloud: str | None = None,
+    ) -> jubilant.Juju:
         """Return a `jubilant.Juju` for a model named `<prefix>-<suffix>`.
 
         `<prefix>` is the factory's configured model-name prefix. If `suffix`
         is empty, the model is named `<prefix>`. The same factory cannot
         return two `Juju` instances for the same model name; raises
         `ValueError` if called twice with the same `suffix`.
+
+        Pass `controller` and/or `cloud` to override the default Juju controller
+        and cloud on a per-model basis. These default to the values passed
+        on the command line (`--juju-controller`/`--juju-cloud`), with Juju falling
+        back to the active controller and cloud if they're not specified.
         """
         ...
 
@@ -169,14 +192,24 @@ class _JujuFactory:
         allow_existing_model: bool = False,
         log_path: pathlib.Path | None = None,
         add_model: bool = False,
+        controller: str | None = None,
+        cloud: str | None = None,
     ):
         self._model_prefix = model_prefix
         self._models: dict[str, jubilant.Juju] = {}
         self._allow_existing_model = allow_existing_model
         self._log_path = log_path
         self._add_model = add_model
+        self._controller = controller
+        self._cloud = cloud
 
-    def get_juju(self, suffix: str) -> jubilant.Juju:
+    def get_juju(
+        self,
+        suffix: str,
+        *,
+        controller: str | None = None,
+        cloud: str | None = None,
+    ) -> jubilant.Juju:
         model_name = f"{self._model_prefix}-{suffix}" if suffix else self._model_prefix
         if model_name in self._models:
             raise ValueError(
@@ -184,10 +217,16 @@ class _JujuFactory:
                 "choose a different prefix."
             )
 
+        # Set controller and cloud for add_model, preferring args to factory defaults
+        juju_controller = controller if controller is not None else self._controller
+        juju_cloud = cloud if cloud is not None else self._cloud
+
+        # Include the controller prefix in the model_name for subsequent Juju calls.
+        model_name = f"{juju_controller}:{model_name}" if juju_controller else model_name
         juju = jubilant.Juju(model=model_name)
         if self._add_model:
             try:
-                juju.add_model(model_name)
+                juju.add_model(model=model_name, cloud=juju_cloud)
             except jubilant.CLIError as e:
                 # If --model is set (_allow_existing_model is True), then the user wants collisions.
                 # If the name is randomly generated, the chance of colliding with another
@@ -217,7 +256,8 @@ class _JujuFactory:
                 end_msg = f"--- end of `juju debug-log` for model {model} ---"
                 print(f"{msg}\n{last_n_lines}\n{end_msg}", file=sys.stderr, flush=True)
             if self._log_path:
-                jdl_path = self._log_path / (model + "-juju-debug.log")
+                model_filename = model.replace(":", "-")
+                jdl_path = self._log_path / (model_filename + "-juju-debug.log")
                 jdl_path.write_text(jdl)
                 logging.info("Wrote full `juju debug-log` for model %s to %s", model, jdl_path)
 
@@ -268,11 +308,15 @@ def juju_factory(
     module_name = typing.cast("str", request.module.__name__)  # type: ignore
     module_part = module_name.rpartition(".")[-1].replace("_", "-")
     dump_logs = typing.cast("pathlib.Path | None", request.config.getoption("--juju-dump-logs"))
+    controller_name = typing.cast("str | None", request.config.getoption("--juju-controller"))
+    cloud_name = typing.cast("str | None", request.config.getoption("--juju-cloud"))
     factory = _JujuFactory(
         model_prefix=f"{_model_prefix}-{module_part}",
         allow_existing_model=bool(request.config.getoption("--juju-model")),
         log_path=dump_logs,
         add_model=not typing.cast("bool", request.config.getoption("--no-juju-setup")),
+        controller=controller_name,
+        cloud=cloud_name,
     )
 
     yield factory

tests/unit/cli_controller_tests.py
@@ -1,0 +1,55 @@
+"""This file is executed via test_cli_controller.py, it should not be run directly."""
+
+from unittest.mock import MagicMock, call
+
+import pytest_jubilant
+
+
+def test_model_is_prefixed_with_controller(
+    cli_mock: MagicMock,
+    juju_factory: pytest_jubilant.JujuFactory,
+):
+    """If ``--juju-controller`` is set, it is used as the model prefix in ``juju add-model``."""
+    cli_mock.reset_mock()
+    juju = juju_factory.get_juju("my-model")
+    assert juju.model == "my-fancy-controller:jubilant-deadbeef-test-file-my-model"
+    assert cli_mock.call_args_list == [
+        call(
+            [
+                "juju",
+                "add-model",
+                "--no-switch",
+                "my-fancy-controller:jubilant-deadbeef-test-file-my-model",
+            ],
+            check=True,
+            capture_output=True,
+            encoding="utf-8",
+            input=None,
+            timeout=None,
+        ),
+    ]
+
+
+def test_get_juju_overrides_controller(
+    cli_mock: MagicMock,
+    juju_factory: pytest_jubilant.JujuFactory,
+):
+    """If JujuFactory is called with a controller argument, it should override the CLI argument."""
+    cli_mock.reset_mock()
+    juju = juju_factory.get_juju("override-model", controller="override-controller")
+    assert juju.model == "override-controller:jubilant-deadbeef-test-file-override-model"
+    assert cli_mock.call_args_list == [
+        call(
+            [
+                "juju",
+                "add-model",
+                "--no-switch",
+                "override-controller:jubilant-deadbeef-test-file-override-model",
+            ],
+            check=True,
+            capture_output=True,
+            encoding="utf-8",
+            input=None,
+            timeout=None,
+        ),
+    ]

tests/unit/test_multicontroller.py
@@ -1,0 +1,96 @@
+from unittest.mock import MagicMock, call
+
+import jubilant
+import pytest
+
+import pytest_jubilant
+
+K8S_CONTROLLER = "k8s-controller"
+MACHINE_CONTROLLER = "lxd-controller"
+MODEL_NAME = "jubilant-deadbeef-test-multicontroller"
+
+
+@pytest.fixture(scope="module")
+def juju_k8s(juju_factory: pytest_jubilant.JujuFactory):
+    """Juju instance for a model on the k8s controller."""
+    yield juju_factory.get_juju(suffix="k8s", controller=K8S_CONTROLLER)
+
+
+@pytest.fixture(scope="module")
+def juju_machine(juju_factory: pytest_jubilant.JujuFactory):
+    """Juju instance for a model on the machine controller."""
+    yield juju_factory.get_juju(suffix="machine", controller=MACHINE_CONTROLLER)
+
+
+def test_multicontroller(
+    cli_mock: MagicMock,
+    juju_k8s: jubilant.Juju,
+    juju_machine: jubilant.Juju,
+):
+    """Test models on different controllers are correctly prefixed."""
+
+    expected_k8s_model = f"{K8S_CONTROLLER}:{MODEL_NAME}-k8s"
+    assert juju_k8s.model == expected_k8s_model
+
+    expected_machine_model = f"{MACHINE_CONTROLLER}:{MODEL_NAME}-machine"
+    assert juju_machine.model == expected_machine_model
+
+    juju_k8s.deploy("k8s-charm")
+    juju_machine.deploy("machine-charm")
+
+    assert cli_mock.call_args_list == [
+        call(
+            [
+                "juju",
+                "add-model",
+                "--no-switch",
+                f"{K8S_CONTROLLER}:{MODEL_NAME}-k8s",
+            ],
+            check=True,
+            capture_output=True,
+            encoding="utf-8",
+            input=None,
+            timeout=None,
+        ),
+        call(
+            [
+                "juju",
+                "add-model",
+                "--no-switch",
+                f"{MACHINE_CONTROLLER}:{MODEL_NAME}-machine",
+            ],
+            check=True,
+            capture_output=True,
+            encoding="utf-8",
+            input=None,
+            timeout=None,
+        ),
+        call(
+            [
+                "juju",
+                "deploy",
+                "--model",
+                f"{K8S_CONTROLLER}:{MODEL_NAME}-k8s",
+                "k8s-charm",
+            ],
+            check=True,
+            capture_output=True,
+            encoding="utf-8",
+            input=None,
+            timeout=None,
+        ),
+        call(
+            [
+                "juju",
+                "deploy",
+                "--model",
+                f"{MACHINE_CONTROLLER}:{MODEL_NAME}-machine",
+                "machine-charm",
+            ],
+            check=True,
+            capture_output=True,
+            encoding="utf-8",
+            input=None,
+            timeout=None,
+        ),
+    ]

tests/unit/cli_cloud_tests.py
@@ -1,0 +1,55 @@
+"""This file is executed via test_cli_cloud.py, it should not be run directly."""
+
+from unittest.mock import MagicMock, call
+
+import pytest_jubilant
+
+
+def test_cloud_is_passed_to_add_model(
+    cli_mock: MagicMock,
+    juju_factory: pytest_jubilant.JujuFactory,
+):
+    """If ``--juju-cloud`` is set, it is passed to ``juju add-model`` as a positional argument."""
+    cli_mock.reset_mock()
+    juju_factory.get_juju("my-model")
+    assert cli_mock.call_args_list == [
+        call(
+            [
+                "juju",
+                "add-model",
+                "--no-switch",
+                "jubilant-deadbeef-test-file-my-model",
+                "my-fancy-cloud",
+            ],
+            check=True,
+            capture_output=True,
+            encoding="utf-8",
+            input=None,
+            timeout=None,
+        ),
+    ]
+
+
+def test_get_juju_overrides_cloud(
+    cli_mock: MagicMock,
+    juju_factory: pytest_jubilant.JujuFactory,
+):
+    """If JujuFactory is called with a cloud argument, it should override the CLI argument."""
+    cli_mock.reset_mock()
+    juju_factory.get_juju("override-model", cloud="override-cloud")
+    assert cli_mock.call_args_list == [
+        call(
+            [
+                "juju",
+                "add-model",
+                "--no-switch",
+                "jubilant-deadbeef-test-file-override-model",
+                "override-cloud",
+            ],
+            check=True,
+            capture_output=True,
+            encoding="utf-8",
+            input=None,
+            timeout=None,
+        ),
+    ]

tests/unit/test_cli_cloud.py
@@ -1,0 +1,18 @@
+from pathlib import Path
+
+import pytest
+
+pytest_plugins = ["pytester"]
+
+CONFTEST = (Path(__file__).parent / "conftest.py").read_text()
+TEST_FILE = (Path(__file__).parent / "cli_cloud_tests.py").read_text()
+
+
+def test_cli_cloud(pytester: pytest.Pytester):
+    """``--juju-cloud`` is used as juju argument, and can be overridden per get_juju() call."""
+    pytester.makeconftest(CONFTEST)
+    pytester.makepyfile(test_file=TEST_FILE)
+
+    result = pytester.runpytest("--juju-cloud", "my-fancy-cloud")
+
+    result.assert_outcomes(passed=2)

tests/unit/test_cli_controller.py
@@ -1,0 +1,18 @@
+from pathlib import Path
+
+import pytest
+
+pytest_plugins = ["pytester"]
+
+CONFTEST = (Path(__file__).parent / "conftest.py").read_text()
+TEST_FILE = (Path(__file__).parent / "cli_controller_tests.py").read_text()
+
+
+def test_cli_controller(pytester: pytest.Pytester):
+    """``--juju-controller`` is used as model prefix, and can be overridden per get_juju() call."""
+    pytester.makeconftest(CONFTEST)
+    pytester.makepyfile(test_file=TEST_FILE)
+
+    result = pytester.runpytest("--juju-controller", "my-fancy-controller")
+
+    result.assert_outcomes(passed=2)

tests/unit/test_multicloud.py
@@ -1,0 +1,92 @@
+from unittest.mock import MagicMock, call
+
+import jubilant
+import pytest
+
+import pytest_jubilant
+
+K8S_CLOUD = "microk8s"
+MACHINE_CLOUD = "localhost"
+MODEL_NAME = "jubilant-deadbeef-test-multicloud"
+
+
+@pytest.fixture(scope="module")
+def juju_k8s(juju_factory: pytest_jubilant.JujuFactory):
+    """Juju instance for a model on the k8s cloud."""
+    yield juju_factory.get_juju(suffix="k8s", cloud=K8S_CLOUD)
+
+
+@pytest.fixture(scope="module")
+def juju_machine(juju_factory: pytest_jubilant.JujuFactory):
+    """Juju instance for a model on the machine cloud."""
+    yield juju_factory.get_juju(suffix="machine", cloud=MACHINE_CLOUD)
+
+
+def test_multicloud(
+    cli_mock: MagicMock,
+    juju_k8s: jubilant.Juju,
+    juju_machine: jubilant.Juju,
+):
+    """Test models on different clouds can be created and deployed to."""
+
+    juju_k8s.deploy("k8s-charm")
+    juju_machine.deploy("machine-charm")
+
+    assert cli_mock.call_args_list == [
+        call(
+            [
+                "juju",
+                "add-model",
+                "--no-switch",
+                f"{MODEL_NAME}-k8s",
+                K8S_CLOUD,
+            ],
+            check=True,
+            capture_output=True,
+            encoding="utf-8",
+            input=None,
+            timeout=None,
+        ),
+        call(
+            [
+                "juju",
+                "add-model",
+                "--no-switch",
+                f"{MODEL_NAME}-machine",
+                MACHINE_CLOUD,
+            ],
+            check=True,
+            capture_output=True,
+            encoding="utf-8",
+            input=None,
+            timeout=None,
+        ),
+        call(
+            [
+                "juju",
+                "deploy",
+                "--model",
+                f"{MODEL_NAME}-k8s",
+                "k8s-charm",
+            ],
+            check=True,
+            capture_output=True,
+            encoding="utf-8",
+            input=None,
+            timeout=None,
+        ),
+        call(
+            [
+                "juju",
+                "deploy",
+                "--model",
+                f"{MODEL_NAME}-machine",
+                "machine-charm",
+            ],
+            check=True,
+            capture_output=True,
+            encoding="utf-8",
+            input=None,
+            timeout=None,
+        ),
+    ]

commit 9b709f35f1f1ee06f98020e2f534f6cd25c879d2

fix: juju add-model not supporting controller prefix in model name (#78)
This fixes a bug introduced in #67 when using multi controller.
Apparently the `juju add-model` is one of the few commands (if not the
only one) that does not support the controller prefix!
This also orders properly the lookup for the model_name (with prefix) in
self._models

Resolves #77

pytest_jubilant/_main.py
@@ -210,23 +210,28 @@ class _JujuFactory:
         controller: str | None = None,
         cloud: str | None = None,
     ) -> jubilant.Juju:
-        model_name = f"{self._model_prefix}-{suffix}" if suffix else self._model_prefix
+        # Set controller and cloud for add_model, preferring args to factory defaults
+        juju_controller = controller if controller is not None else self._controller
+        juju_cloud = cloud if cloud is not None else self._cloud
+
+        short_model_name = f"{self._model_prefix}-{suffix}" if suffix else self._model_prefix
+
+        # Include the controller prefix in the model_name for subsequent Juju calls.
+        model_name = (
+            f"{juju_controller}:{short_model_name}" if juju_controller else short_model_name
+        )
         if model_name in self._models:
             raise ValueError(
                 f"model {model_name} already registered on this juju_factory. "
                 "choose a different prefix."
             )
-
-        # Set controller and cloud for add_model, preferring args to factory defaults
-        juju_controller = controller if controller is not None else self._controller
-        juju_cloud = cloud if cloud is not None else self._cloud
-
-        # Include the controller prefix in the model_name for subsequent Juju calls.
-        model_name = f"{juju_controller}:{model_name}" if juju_controller else model_name
         juju = jubilant.Juju(model=model_name)
         if self._add_model:
             try:
-                juju.add_model(model=model_name, cloud=juju_cloud)
+                # juju add-model does not support the <controller>:<model> syntax
+                juju.add_model(
+                    model=short_model_name, cloud=juju_cloud, controller=juju_controller
+                )
             except jubilant.CLIError as e:
                 # If --model is set (_allow_existing_model is True), then the user wants collisions.
                 # If the name is randomly generated, the chance of colliding with another

tests/integration/conftest.py
@@ -24,3 +24,8 @@ def log_actions_charm():
     if not charm_path.exists():
         raise FileNotFoundError(f"charm not found at {charm_path}")
     return charm_path.absolute()
+
+
+@pytest.fixture(scope="session")
+def juju_controller():
+    return os.environ.get("JUJU_CONTROLLER", "concierge-lxd")

tests/integration/test_juju_factory.py
@@ -1,0 +1,44 @@
+import pathlib
+
+import jubilant
+import pytest
+
+import pytest_jubilant
+
+
+@pytest.fixture(scope="module")
+def juju_one(juju_factory: pytest_jubilant.JujuFactory):
+    """Juju instance for a model on the machine model."""
+    yield juju_factory.get_juju(suffix="one")
+
+
+@pytest.fixture(scope="module")
+def juju_two(juju_factory: pytest_jubilant.JujuFactory, juju_controller: str):
+    """Juju instance for a model on the machine model with specified controller."""
+    yield juju_factory.get_juju(suffix="two", controller=juju_controller)
+
+
+def test_juju_factory_deploy_charm(
+    smoke_charm: pathlib.Path,
+    juju_one: jubilant.Juju,
+    juju_controller: str,
+):
+    """Test application is properly deployed when using JujuFactory."""
+    juju_one.deploy(smoke_charm)
+    juju_one.wait(jubilant.all_active, timeout=60 * 30)
+
+    # Given we didn't specify a controller, the model name should not be prefixed
+    assert juju_one.model is not None and not juju_one.model.startswith(f"{juju_controller}:")
+
+
+def test_juju_factory_prefix_controller_in_model(
+    smoke_charm: pathlib.Path,
+    juju_two: jubilant.Juju,
+    juju_controller: str,
+):
+    """Test model name is correctly prefixed with controller when using JujuFactory."""
+    juju_two.deploy(smoke_charm)
+    juju_two.wait(jubilant.all_active, timeout=60 * 30)
+
+    # Given we specified a controller, the model name should be prefixed with it
+    assert juju_two.model is not None and juju_two.model.startswith(f"{juju_controller}:")

tests/unit/cli_controller_tests.py
@@ -19,7 +19,9 @@ def test_model_is_prefixed_with_controller(
                 "juju",
                 "add-model",
                 "--no-switch",
-                "my-fancy-controller:jubilant-deadbeef-test-file-my-model",
+                "jubilant-deadbeef-test-file-my-model",
+                "--controller",
+                "my-fancy-controller",
             ],
             check=True,
             capture_output=True,
@@ -44,7 +46,9 @@ def test_get_juju_overrides_controller(
                 "juju",
                 "add-model",
                 "--no-switch",
-                "override-controller:jubilant-deadbeef-test-file-override-model",
+                "jubilant-deadbeef-test-file-override-model",
+                "--controller",
+                "override-controller",
             ],
             check=True,
             capture_output=True,

tests/unit/test_multicontroller.py
@@ -44,7 +44,9 @@ def test_multicontroller(
                 "juju",
                 "add-model",
                 "--no-switch",
-                f"{K8S_CONTROLLER}:{MODEL_NAME}-k8s",
+                f"{MODEL_NAME}-k8s",
+                "--controller",
+                K8S_CONTROLLER,
             ],
             check=True,
             capture_output=True,
@@ -57,7 +59,9 @@ def test_multicontroller(
                 "juju",
                 "add-model",
                 "--no-switch",
-                f"{MACHINE_CONTROLLER}:{MODEL_NAME}-machine",
+                f"{MODEL_NAME}-machine",
+                "--controller",
+                MACHINE_CONTROLLER,
             ],
             check=True,
             capture_output=True,